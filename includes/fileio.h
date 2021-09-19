47ac36aae5122df95f9a08fff1a61862820236b2
Jonathan Kaufman <jonathan.kaufman.18@cnu.edu>
2021-09-18 22:00:42 -0400

    Update

---
+++ includes/fileio.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int load(const std::string filename, std::vector<process> &myProcesses);
+
+int save(const std::string filename, std::vector<process> &myProcesses);
